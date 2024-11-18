b4901d2983f04f6b92e8d23a2b18af71c93c90a7
kishoredarla <kishoredarla60@gmail.com>
2024-11-18 23:52:07 +0000

    pug engine setup

--- app/app.js
+++ app/app.js
@@ -10,6 +10,10 @@ app.use(express.static("static"));
 // Get the functions in the db.js file to use
 const db = require('./services/db');
 
+// Use the Pug templating engine
+app.set('view engine', 'pug');
+app.set('views', './app/views');
+
 // Create a route for root - /
 app.get("/", function(req, res) {
     res.send("Hello world!");
